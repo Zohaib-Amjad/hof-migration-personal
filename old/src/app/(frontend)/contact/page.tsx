1b2f5ce5e7c21df726ab4b9317164829a0e1a510
Zohaib Amjad <zohaibamjad@hof-global.com>
2026-07-14 13:08:06 +0500

    refactor: remove deprecated and unused utility files

--- old/src/app/(frontend)/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-  return (
-    <div className="container mx-auto px-4 py-12">
-      {/* TODO: content */}
-      <h1 className="text-3xl font-semibold">Contact</h1>
-    </div>
-  )
-}
--- old/src/app/(frontend)/services/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ServicesPage() {
-  return (
-    <div className="container mx-auto px-4 py-12">
-      {/* TODO: content */}
-      <h1 className="text-3xl font-semibold">Services</h1>
-    </div>
-  )
-}
